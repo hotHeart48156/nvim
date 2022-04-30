6f43522c4b388903908e6c1104676995f46f0113
HotHeart48156 <000111aaaim@gmail.com>
2022-04-30 19:01:39 +0800

    update

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -29,46 +29,25 @@ function print_r ( t )
     sub_print_r(t,"  ")
 end
 
-
-local packages={
-
-basic = 'basic',
-lsp = 'nvim_cmp',
-themes = 'themes',
-tree = 'treesitter',
-telescope = 'telescope',
-debug = 'debug',
-git = 'git',
-markdown = 'markdown',
-snippets = "snippets",
-}
-
-for name,package in pairs(packages) do
-	packages[name]=package.."."
-end
-
 local plugins= {}
---https://gitter.im/neovim/neovim/archives/2019/11/16
 local luv=require 'luv'
--- print(vim.o.runtimepath) return string
--- print(vim.opt.runtimepath:get()) api local way
--- print(vim.api.nvim_list_runtime_paths()) --api way
+local all_plugin_configure_file={}
 local nvim_config_path=vim.api.nvim_list_runtime_paths()[1]..'/lua/'..plugins_configure.plugin_configure_root:sub(1,-2)
 local fs_t=luv.fs_scandir(nvim_config_path)
 local function traverse_directory (root_path,super_node,node)
 
 	local file_name,file_type=luv.fs_scandir_next(node)
 
-	if file_type=='directory' then
+	if file_type=='directory' and file_name:sub(1,1)~='_'  then
 	   local old_node=node
 	   local new_node=luv.fs_scandir(root_path..'/'..file_name)
            local super_path=root_path..'/'..file_name
-           print('entry directory:'..super_path)
+           --print('entry directory:'..super_path)
 	   traverse_directory(super_path,old_node,new_node)
 	end
 
-	if file_type=='file' then 
-	   print(root_path..'/'..file_name)
+	if file_type=='file' and file_name:sub(1,1)~='_' then 
+           table.insert(all_plugin_configure_file,root_path..'/'..file_name)  
 	   traverse_directory(root_path,super_node,node)
 	end
 
@@ -80,69 +59,37 @@ local function traverse_directory (root_path,super_node,node)
                 local i=root_path:match('.*'..'/()')
                 local super_path=root_path:sub(1,i-2)
                 local super_fs=super_node
-                print('return directory :'..super_path)
-
+                --print('return directory :'..super_path)
 		traverse_directory(super_path,fs_t,super_fs)
         end
 end
---local fs_t=luv.fs_scandir(nvim_config_path)
 traverse_directory(nvim_config_path,nil,fs_t)
 
-plugins[packages['basic']] = {
-   packages['basic'].."nerd_commenter",       -- for quick comment
-   packages['basic'].."ultisnips",
-   packages['basic']..'auto_pairs',
-  }
 
-plugins[packages['lsp']] = {
- packages['lsp']..'lsp_config',
- packages['lsp']..'nvim_cmp',
- packages['lsp']..'cmp_buffer',
- packages['lsp']..'cmp_cmdline',
- packages['lsp']..'cmp_lua',
- packages['lsp']..'cmp_luasnip',
- packages['lsp']..'cmp_path',
- packages['lsp']..'cmp_spell',
- packages['lsp']..'cmp_tabnine'
-}
+for _,path in ipairs(all_plugin_configure_file) do
+   local cut_attribute_path=path:sub(1,-5)
+   local configure_index=cut_attribute_path:match('.*configure()')
+   local path_len=cut_attribute_path:len()
+   local package_sort_path=cut_attribute_path:sub(configure_index-path_len)
+   local header_index=package_sort_path:match('.*/()')
+   if header_index==nil then
+   	header_index=package_sort_path:len()
+   end
+   local header=package_sort_path:sub(1,header_index-2)
+   local header=header:gsub('/','.') 
+   local package=package_sort_path:gsub('/','.')
+   if  plugins[header]==nil then
+        plugins[header]={}
+   end
+   table.insert( plugins[header],package)
+end
 
-plugins[packages['themes']] = {
-   packages['themes']..'material',
-   packages['basic']..'dashboard',
-   packages['themes']..'colorizer'
-}
 
-plugins[packages['tree']] = {
-   packages['tree']..'nvim_treesitter'
-}
-
-plugins[packages['telescope']] = {
-   packages['telescope']..'telescope',
-  -- packages['telescope']..'telescope_fzf_native',
-  -- packages['telescope']..'telescope_live_grep_raw',
-  -- packages['telescope']..'telescope_ui_select',
-  -- packages['telescope']..'telescope_vim_bookmarks',
-   packages['telescope']..'vim_bookmarks'
-}
-
-plugins[packages['debug']] = {
-   packages['debug']..'sniprun'
-}
-
-plugins[packages['git']] = {
-   packages['git']..'git_signs',
-   packages['git']..'diff_view',
-}
-
-plugins[packages['snippets']] = 
-{
-   packages['snippets']..'friendly-snippert',
-   packages['snippets']..'sources_lua_snip'
-}
-
-plugins[packages['markdown']] = {
-   packages['markdown']..'markdown_image'
-}
+for key,value in pairs(plugins) do
+    for k,v in ipairs(value) do
+    	print("header:"..key..'package:'..v)
+    end
+end
 
 
 for feature_name,plugin_s in pairs(plugins) do
@@ -203,7 +150,6 @@ plugins_configure.setup=function()
         for feature_name,plugins in pairs (plugins_configure.plugins_groups) do
             if FEATURES[feature_name:sub(1,-2)]==true then
                 for plugin , plugin_status in pairs(plugins) do 
-		    print(plugin)
                     core = require(plugins_configure.plugin_configure_root..plugin).core
                     for k,v in pairs(plugin_status) do 
                        core[k] = v  
