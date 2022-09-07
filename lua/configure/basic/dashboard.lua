4da058a9194113f96081a1bafcb741b887a62ca1
hotHeart48156 <000111aaaim@gmail.com>
2022-09-07 23:20:52 +0800

    update

--- lua/configure/basic/dashboard.lua
+++ lua/configure/basic/dashboard.lua
@@ -1,33 +1,23 @@
 local plugin = {}
-plugin.core={"glepnir/dashboard-nvim"}
-
-plugin.core.disable=false
-plugin.core.opt=false
+plugin.core = {"glepnir/dashboard-nvim"}
 plugin.core.setup = function()
-        vim.g.dashboard_default_executive = 'telescope'
-        vim.g.dashboard_preview_command = 'cat'
-        vim.g.dashboard_preview_pipeline = 'lolcat'
-        vim.g.dashboard_preview_file_height = 10
-        vim.g.dashboard_preview_file_width = 70
-       -- vim.g.dashboard_preview_file = vim.g.CONFIG..'/lua/util/neovim.txt'
-       -- tips vim.g.CONFIG is nil 
 end
 
 plugin.core.config = function()
-    vim.g.dashboard_custom_footer = {"🐬 Have A Good Day!"}
-    vim.g.dashboard_custom_shortcut = {
-            last_session       = 'SPC s l',
-            find_history       = 'SPC f h',
-            find_file          = 'SPC f f',
-            new_file           = 'SPC c f',
-            change_colorscheme = 'SPC f c',
-            find_word          = 'SPC f q',
-            book_marks         = 'SPC f m',
-        }
-    vim.g.dashboard_session_directory = vim.fn.stdpath('cache').."/session"
+    vim.g.dashboard_session_directory = vim.fn.stdpath('cache') .. "/session"
+    local db = require('dashboard')
+    db.custom_center = {{
+        icon = '  ',
+        desc = 'Recently latest session                  ',
+        shortcut = 'SPC s l',
+        action = 'SessionLoad'
+    }}
+    db.custom_header={"yes today"}
+    db.custom_footer = {"🐬 Have A Good Day!"}
+
 end
 
-plugin.mapping=function()
+plugin.mapping = function()
 end
 
 return plugin
