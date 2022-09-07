d49469ddb9133413e8d7e681a76a782b8169a847
hotHeart48156 <000111aaaim@gmail.com>
2022-09-07 22:22:51 +0800

    update

--- lua/configure/debug/config/_dap_cpp.lua
+++ lua/configure/debug/config/_dap_cpp.lua
@@ -54,7 +54,7 @@ dap.configurations.cpp = {
     request = 'launch',
     MIMode = 'gdb',
     miDebuggerServerAddress = 'localhost:1234',
-    miDebuggerPath = '/usr/bin/gdb', cwd = '${workspaceFolder}',
+    miDebuggerPath = '/usr/bin/lldb', cwd = '${workspaceFolder}',
     program = function()
       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
     end,
